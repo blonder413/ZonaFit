866fff0a39cdf117760ae4a2187c2213415f7d94
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-11-09 17:09:10 -0500

    listar clientes

---
+++ src/main/java/zona_fit/datos/ClienteDAO.java
@@ -0,0 +1,70 @@
+package zona_fit.datos;
+
+import zona_fit.dominio.Cliente;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import static zona_fit.conexion.Conexion.getConexion;
+
+public class ClienteDAO implements IClienteDAO {
+    @Override
+    public List<Cliente> listarClientes() {
+        List<Cliente> clientes = new ArrayList<>();
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection con = getConexion();
+        String sql = "SELECT * FROM cliente ORDER BY id";
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Cliente cliente = new Cliente();
+                cliente.setId(rs.getInt("id"));
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setMembresia(rs.getInt("membresia"));
+                clientes.add(cliente);
+            }
+        } catch (Exception e) {
+            System.out.println("Error al listar clientes: " + e.getMessage());
+        } finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error al cerrar conexión: " + e.getMessage());
+            }
+        }
+        return clientes;
+    }
+
+    @Override
+    public boolean buscarClientePorId(Cliente cliente) {
+        return false;
+    }
+
+    @Override
+    public boolean agregarClietne(Cliente cliente) {
+        return false;
+    }
+
+    @Override
+    public boolean modificarCliente(Cliente cliente) {
+        return false;
+    }
+
+    @Override
+    public boolean eliminarCliente(Cliente cliente) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Clientes: ");
+        ClienteDAO clienteDao = new ClienteDAO();
+        List<Cliente> clientes = clienteDao.listarClientes();
+        clientes.forEach(System.out::println);
+    }
+}
