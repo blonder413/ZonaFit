4e2ee9deb5282c1644448f81ece775e491d12522
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-11-09 15:30:41 -0500

    conflicto

---
+++ src/main/java/zona_fit/conexion/Conexion.java
@@ -0,0 +1,30 @@
+package zona_fit.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+    public static Connection getConexion() {
+        Connection conexion = null;
+        String baseDatos = "zona_fit";
+        String url = "jdbc:mysql://localhost:3306/" + baseDatos;
+        String usuario = "root";
+        String password = "password";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, usuario, password);
+        } catch (Exception e) {
+            System.out.println("Error al conectarnos a la base de datos: " + e.getMessage());
+        }
+        return conexion;
+    }
+
+    public static void main(String[] args) {
+        var conexion = Conexion.getConexion();
+        if (conexion!=null){
+            System.out.println("Conexión exitosa: " + conexion);
+        } else {
+            System.out.println("Error al conectarse.");
+        }
+    }
+}
